a288880b86b526233cb332bc9060d92791027737
Matt Oakes <hello@mattoakes.net>
2023-04-17 08:12:58 +0100

    Remove the code specific to the old router

--- dhclient/install-service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo rm /etc/systemd/system/dhclient-refresh.{service,timer}
-sudo cp -f dhclient-refresh.service dhclient-refresh.timer /etc/systemd/system/
-sudo systemctl stop dhclient-refresh.service
-sudo systemctl daemon-reload
-sudo systemctl enable dhclient-refresh.timer dhclient-refresh.service
-sudo systemctl start dhclient-refresh.service
-sudo systemctl daemon-reload
